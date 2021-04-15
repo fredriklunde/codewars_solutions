db44b4b9de63370c4d7d5baba87451c9456087ac
Fredrik Lunde <fredrik.lunde@frontedgeit.se>
2021-04-15 20:31:39 +0200

    Isograms

---
+++ src/Isograms/test.js
@@ -0,0 +1,10 @@
+const Test = require("@codewars/test-compat");
+const isIsogram = require('./solution');
+
+
+Test.assertSimilar( isIsogram("Dermatoglyphics"), true );
+Test.assertSimilar( isIsogram("isogram"), true );
+Test.assertSimilar( isIsogram("aba"), false, "same chars may not be adjacent" );
+Test.assertSimilar( isIsogram("moOse"), false, "same chars may not be same case" );
+Test.assertSimilar( isIsogram("isIsogram"), false );
+Test.assertSimilar( isIsogram(""), true, "an empty string is a valid isogram" );
